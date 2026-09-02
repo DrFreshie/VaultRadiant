efa678f381e1a9ed97f5ecf82e058233a1b5d7fb
Anders Thaysen <andersthaysen@MacBook-Air-tilhrende-Anders.local>
2026-09-02 15:23:40 +0200

    Quartz sync: Sep 2, 2026, 3:23 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -20,7 +20,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", ".templates", ".obsidian", "dailies"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
